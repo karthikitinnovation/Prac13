6ad6d591886b81c3addc0a1a9a6bcfd5ddf3073b
Karthik <karthikitinnovation@gmail.com>
2018-09-28 08:02:55 +0530

    created folder screenshot and also the new file of screenshot in utility

--- src/test/java/utility/Screenshot.java
+++ src/test/java/utility/Screenshot.java
@@ -1,5 +1,30 @@
 package utility;
 
-public class Screenshot {
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Screenshot 
+{
+	public static void capturescreenshot(WebDriver driver, String nameofscreenshot) throws IOException
+	{
+		try
+		{
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(source, new File (".//screenshot/"+nameofscreenshot+".png"));
+		
+		System.out.println(nameofscreenshot+ "captured successfully");
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("screen shot NOT captured becos "+e.getMessage());
+		}
+		
+	}
 }
